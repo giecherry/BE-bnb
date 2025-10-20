063eae24608167037366ac9eb65c4eaeb045a8fe
Angie C <a.cajusol98@gmail.com>
2025-10-20 16:50:57 +0200

    sb

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,17 @@
-import { createClient } from "@supabase/supabase-js"
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL as string
-const supabaseApiKey = process.env.SUPABASE_ANON_KEY as string
 
-if (!supabaseUrl || !supabaseApiKey) {
-  throw new Error("Missing Supabase credentials")
+const _supabaseUrl = process.env.SUPABASE_URL;
+const _supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!_supabaseUrl || !_supabaseAnonKey) {
+  throw new Error(
+    "Supabase not initalized add 'SUPABASE_URL' and 'SUPABASE_ANON_KEY' to enviroment variables"
+  );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseApiKey)
-export { supabaseUrl, supabaseApiKey }
+const supabaseUrl = _supabaseUrl as string
+const supabaseAnonKey = _supabaseAnonKey as string
+
+export { supabaseUrl, supabaseAnonKey }
