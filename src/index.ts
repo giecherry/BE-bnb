9e178261b72924aaa4fbf1a528fb6216432ea0ad
Angie C <a.cajusol98@gmail.com>
2025-11-05 18:08:29 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = new Hono();
 app.use(
     "*",
     cors({
-        origin: ["http://localhost:3000"],
+        origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
         allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowHeaders: ["Content-Type", "Authorization"],
         credentials: true,
